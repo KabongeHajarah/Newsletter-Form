bf1be0b5bda3f76efeee81a762109d6b05624e31
KabongeHajarah <kabongehajarah@gmail.com>
2023-07-02 19:08:43 +0400

    Adding functionality

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ heading.style.color='#000033'
 heading.style.fontSize='60px'
 heading.style.fontWeight='bold'
 
+
+document.getElementById('subscriptionForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('emailInput').value;
+
+    // Validate email format
+    if (validateEmail(email)) {
+      // Redirect to success page
+      window.location.href = 'success.html?email=' + encodeURIComponent(email);
+    }
+  });
+  function validateEmail(email) {
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    return emailRegex.test(email);
+  }
+
+ 
